1c9b99045edf95ac794858d9630f2219a4d2179e
SN19Y-MRT <yuuki.karate1ban@icloud.com>
2022-01-25 23:40:38 +0900

    カテゴリー追加

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class UserTableSeeder extends Seeder
+class CategoriesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,9 +11,9 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('categories')->insert([
+            DB::table('categories')->insert([
             'id' => 1,
             'name' => 'トップス',
-        ]);
+            ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,6 @@ use Illuminate\Http\Request;
 //    return $request->user();
 //});
 
-Route::post('login', 'API\UserController@login');
-Route::post('register', 'API\UserController@register');
 
 
 Route::group(['middleware' => 'auth:api'], function(){
