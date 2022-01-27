1ae92f49cc9f00bd94b804a7eabcd7137a3e6d60
SN19Y-MRT <yuuki.karate1ban@icloud.com>
2022-01-27 13:51:30 +0900

    push error

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,5 @@ use Illuminate\Http\Request;
 */
 
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
+  //  return $request->user();
 //});
-
-Route::group(['middleware' => 'auth:api'], function(){
-    Route::post('details', 'API\UserController@details');
-});
