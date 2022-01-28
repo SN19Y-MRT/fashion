bbf8da40f57d5ed407f00d70df276e36f23f14d6
SN19Y-MRT <yuuki.karate1ban@icloud.com>
2022-01-28 14:48:48 +0900

    カテゴリーデータベース追加

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -12,111 +12,111 @@ class CategoriesTableSeeder extends Seeder
     public function run()
     {
         DB::table('categories')->insert([
-            'id' => 1,
+            'id' => 2,
             'name' => 'シャツ',
         ]);
         DB::table('categories')->insert([
-            'id' => 2,
+            'id' => 3,
             'name' => 'ブラウス',
         ]);
         DB::table('categories')->insert([
-            'id' => 3,
+            'id' => 4,
             'name' => 'Tシャツ',
         ]);
         DB::table('categories')->insert([
-            'id' => 4,
+            'id' => 5,
             'name' => 'カットソー',
         ]);
         DB::table('categories')->insert([
-            'id' => 5,
+            'id' => 6,
             'name' => 'タンクトップ',
         ]);
         DB::table('categories')->insert([
-            'id' => 6,
+            'id' => 7,
             'name' => 'キャミソール',
         ]);
         DB::table('categories')->insert([
-            'id' => 7,
+            'id' => 8,
             'name' => 'スウェット',
         ]);
         DB::table('categories')->insert([
-            'id' => 8,
+            'id' => 9,
             'name' => 'パーカー',
         ]);
         DB::table('categories')->insert([
-            'id' => 9,
+            'id' => 10,
             'name' => 'ニット/セーター',
         ]);
         DB::table('categories')->insert([
-            'id' => 10,
+            'id' => 11,
             'name' => 'カーディガン',
         ]);
         DB::table('categories')->insert([
-            'id' => 11,
+            'id' => 12,
             'name' => 'ジャケット',
         ]);
         DB::table('categories')->insert([
-            'id' => 12,
+            'id' => 13,
             'name' => 'ブルゾン',
         ]);
         DB::table('categories')->insert([
-            'id' => 13,
+            'id' => 14,
             'name' => 'ベスト',
         ]);
         DB::table('categories')->insert([
-            'id' => 14,
+            'id' => 15,
             'name' => 'コート',
         ]);
         DB::table('categories')->insert([
-            'id' => 15,
+            'id' => 16,
             'name' => 'ボトムス',
         ]);
         DB::table('categories')->insert([
-            'id' => 16,
+            'id' => 17,
             'name' => 'スカート',
         ]);
         DB::table('categories')->insert([
-            'id' => 17,
+            'id' => 18,
             'name' => 'ワンピース',
         ]);
         DB::table('categories')->insert([
-            'id' => 18,
+            'id' => 19,
             'name' => 'バッグ',
         ]);
         DB::table('categories')->insert([
-            'id' => 19,
+            'id' => 20,
             'name' => '靴',
         ]);
         DB::table('categories')->insert([
-            'id' => 20,
+            'id' => 21,
             'name' => 'ファッション雑貨',
         ]);
         DB::table('categories')->insert([
-            'id' => 21,
+            'id' => 22,
             'name' => 'サングラス',
         ]);
         DB::table('categories')->insert([
-            'id' => 22,
+            'id' => 23,
             'name' => '財布、小物',
         ]);
         DB::table('categories')->insert([
-            'id' => 23,
+            'id' => 24,
             'name' => '腕時計',
         ]);
         DB::table('categories')->insert([
-            'id' => 24,
+            'id' => 25,
             'name' => 'アクセサリー',
         ]);
         DB::table('categories')->insert([
-            'id' => 25,
+            'id' => 26,
             'name' => '帽子',
         ]);
         DB::table('categories')->insert([
-            'id' => 26,
+            'id' => 27,
             'name' => 'コスメ、香水',
         ]);
         DB::table('categories')->insert([
-            'id' => 27,
+            'id' => 28,
             'name' => 'その他',
         ]);
 
