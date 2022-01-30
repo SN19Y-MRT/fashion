ae6065de07d015dfcbe0d66dbbe92e78e0b4af04
SN19Y-MRT <yuuki.karate1ban@icloud.com>
2022-01-30 23:10:56 +0900

    heroku push 4

--- app/Http/Requests/fashionRequest.php
+++ app/Http/Requests/fashionRequest.php
@@ -9,8 +9,8 @@ class fashionRequest extends FormRequest
         public function rules()
     {
         return [
-            'fashion.fashionName' => 'required|string|max:100',
-            'fashion.fashionOverview' => 'required|string|max:4000',
+            'fashionName' => 'required|string|max:100',
+            'fashionOverview' => 'required|string|max:4000',
             'image' => 'mimes:jpeg,jpg,png,gif|max:10240',
         ];
     }
