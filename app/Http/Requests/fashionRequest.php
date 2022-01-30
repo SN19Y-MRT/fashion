8153519fad1be4354d07ed8defdf28f5f04fd243
SN19Y-MRT <yuuki.karate1ban@icloud.com>
2022-01-30 23:27:15 +0900

    画像保存エラー1

--- app/Http/Requests/fashionRequest.php
+++ app/Http/Requests/fashionRequest.php
@@ -6,23 +6,22 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class fashionRequest extends FormRequest
 {
-        public function rules()
+    public function rules()
     {
         return [
-            'fashionName' => 'required|string|max:100',
-            'fashionOverview' => 'required|string|max:4000',
+            'fashion.fashionName' => 'required|string|max:100',
+            'fashion.fashionOverview' => 'required|string|max:4000',
             'image' => 'mimes:jpeg,jpg,png,gif|max:10240',
         ];
     }
-
+    
         public function messages()
     {
         return [
-            'fashionName.required' => 'ファッションアイテムネームの入力は必須です。',
-            'fashionOverview.required'  => 'ファッションアイテムの概要は必須です。',
+            'fashionName.required' => 'fashion item Namは必須です。',
+            'fashionOverview.required'  => 'fashion itemの概要は必須です。',
             'image.mimes'    => 'ファイルタイプをjpeg,jpg,png,gifに設定してください。',
             'image.max'      => 'ファイルサイズを10MB以下に設定してください。',
         ];
     }
-
 }
