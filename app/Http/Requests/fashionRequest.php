d9b229f0716fa1cccc0fa5ae751a0c83c05ea60a
SN19Y-MRT <yuuki.karate1ban@icloud.com>
2022-01-31 18:45:15 +0900

    バリデーション2

--- app/Http/Requests/fashionRequest.php
+++ app/Http/Requests/fashionRequest.php
@@ -11,7 +11,7 @@ class fashionRequest extends FormRequest
         return [
             'fashion.fashionName' => 'required|string|max:100',
             'fashion.fashionOverview' => 'required|string|max:4000',
-            'image' => 'mimes:jpeg,jpg,png,gif|max:10240',
+        
         ];
     }
 
@@ -20,8 +20,7 @@ class fashionRequest extends FormRequest
         return [
             'fashionName.required' => 'ファッションアイテムネームの入力は必須です。',
             'fashionOverview.required'  => 'ファッションアイテムの概要は必須です。',
-            'image.mimes'    => 'ファイルタイプをjpeg,jpg,png,gifに設定してください。',
-            'image.max'      => 'ファイルサイズを10MB以下に設定してください。',
+
         ];
     }
 }
